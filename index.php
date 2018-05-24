3eca23afbb1d0c3e8463b629aecbe94d287c814b
takahashi0127 <justhumannohero0127@gmail.com>
2018-05-24 15:31:14 +0900

    rename

--- index.php
+++ index.php
@@ -19,18 +19,11 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('LineMessageAPI
 // イベントタイプがmessage以外はスルー
 if ($event->type != "message")
     return;
+
 $replyMessage = null;
 // メッセージタイプが文字列の場合
 if ($event->message->type == "text") {
-    //$replyMessage = $event->message->text;
-    //docomo返信
-    $res = $bot->getProfile($event->source->userId);
-    if ($res->isSucceeded()) {
-      $userProfile = $res->getJSONDecodedBody();
-      $displayName = $userProfile['displayName'];
-    }
-    $replyMessage = chat($event->message->text, $event->source->userId, $displayName);
-
+    $replyMessage = $event->message->text;
 }
 //文字列以外は無視
 else {
@@ -45,28 +38,3 @@ $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
 var_export($response, true);
 error_log(var_export($response,true));
 return;
-
-
-//ドコモの雑談APIから雑談データを取得
-//From "https://qiita.com/Yuta_Fujiwara/items/281d3e36845b37872a16"
-function chat($text, $userID, $displayName) {
-    // docomo chatAPI
-    $api_key = getenv('docomoAPIKey');
-    $api_url = sprintf('https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=%s', $api_key);
-    $req_body = array('utt' => $text, 'context' => $userID, 'nickname' => $displayName, 'place' => '松江');
-
-    $headers = array(
-        'Content-Type: application/json; charset=UTF-8',
-    );
-    $options = array(
-        'http'=>array(
-            'method'  => 'POST',
-            'header'  => implode("\r\n", $headers),
-            'content' => json_encode($req_body),
-            )
-        );
-    $stream = stream_context_create($options);
-    $res = json_decode(file_get_contents($api_url, false, $stream));
-    error_log($res->utt);
-    return $res->utt;
-}
