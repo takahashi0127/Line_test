2dd7660f90d8ebab9f55580d2f34a03ea1949bab
takahashi0127 <justhumannohero0127@gmail.com>
2018-06-06 16:32:12 +0900

    beacon test

--- index.php
+++ index.php
@@ -59,17 +59,21 @@ foreach ($events as $event) {
     if ($event->message->type == "text") { //テキストメッセージの場合
         if($event->message->text == "ヘルプ"){
             $replyMessage = "1：鍵の登録"."\n"."2：施錠確認開始"."\n"."3：施錠状況確認";
+
+            if($event->message->text == "1"){
+                $replyMessage = "鍵の名前を入力してください";
+                if($event->message->type == "text"){
+		  $keyname = $event->message->text;
+                  $replyMessage = "$keyname";
+                }
+             }
+            else{
+		return;
+	    }
         }
-        else if($event->message->text == "1"){
-            $replyMessage = "鍵の名前を入力してください";
-        //    if($event->message->type == "text"){
-	//	$keyname = $event->message->text;
-        //        $replyMessage = "$keyname";
-        //    }
-        //    else{
-	//	return;
-	//  }
-        }
+
+    }
+
         else if ($event->message->text != "ありがとう"){
             $replyMessage = $event->message->text;
         }
