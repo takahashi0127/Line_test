8cbb84d923d390692846d9fd4926337498f363a0
takahashi0127 <justhumannohero0127@gmail.com>
2018-06-13 12:22:16 +0900

    beacon test

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 ﻿<?php
-require_once __DIR__ . '/vendor/autoload.php';
+/*require_once __DIR__ . '/vendor/autoload.php';
 
 error_log("start");
 
@@ -112,4 +112,34 @@ $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($reply
 $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
 var_export($response, true);
 error_log(var_export($response,true));
-return;
+return;*/
+
+define("LINE_MESSAGING_API_CHANNEL_SECRET", 'your channel secret');
+define("LINE_MESSAGING_API_CHANNEL_TOKEN", 'your channel token');
+
+require __DIR__."/../vendor/autoload.php";
+
+$bot = new \LINE\LINEBot(
+    new \LINE\LINEBot\HTTPClient\CurlHTTPClient(LINE_MESSAGING_API_CHANNEL_TOKEN),
+    ['channelSecret' => LINE_MESSAGING_API_CHANNEL_SECRET]
+);
+
+$signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+$body = file_get_contents("php://input");
+
+$events = $bot->parseEventRequest($body, $signature);
+
+foreach ($events as $event) {
+    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
+        $reply_token = $event->getReplyToken();
+        $text = $event->getText();
+        $bot->replyText($reply_token, $text);
+/* ================= ここから追記 =============================*/
+    } elseif ($event instanceof \LINE\LINEBot\Event\BeaconDetectionEvent) {
+        $reply_token = $event->getReplyToken();
+        $bot->replyText($reply_token, "近くにいますね？");
+/* ================= ここまで追記 =============================*/
+    }
+}
+
+echo "OK";
