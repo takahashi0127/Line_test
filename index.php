96274d8fd672c500867dbe59be1e3ff6eed87622
takahashi0127 <justhumannohero0127@gmail.com>
2018-06-13 18:02:53 +0900

    beacon test

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ foreach ($events as $event) {
 ///////メッセージタイプが文字列の場合////////////
     else if ($event->message->type == "text") {
         if ($event->message->text == "ヘルプ"){
-            $replyMessage = "1：鍵の登録"."\n"."2：施錠確認開始"."\n"."3：施錠状況確認";
+            $replyMessage = "数字を入力してください\n1：鍵の登録\n2：施錠確認開始\n3：施錠状況確認";
 
             /*if ($event->message->text == "1"){
                 $replyMessage = "鍵の名前を入力してください";
