920385ff86dc348f010c067444b3f9327018382a
takahashi0127 <justhumannohero0127@gmail.com>
2018-06-15 13:11:56 +0900

    beacon test

--- index.php
+++ index.php
@@ -44,20 +44,23 @@ foreach ($events as $event) {
 
 
 /////////////////////////ビーコンイベント///////////////////////////////////
+    $beaconevent = $event->beacon->type; //enter or leave
 
     if (!empty($event->beacon)) {
         $type = $event->beacon->type; //enter or leave
 
-        if ($type === "enter"){
+
+        if ($beaconevent === "enter"){
             $beaconMessage = "おかえりなさい\n戸締りの確認をしましょう";
             $beaconMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($beaconMessage);
             $response = $bot->replyMessage($event->replyToken, $beaconMessageBuilder);
         }
-        else if (($type === "leave")) {
+        else if (($beaconevent === "leave")) {
             $beaconMessage = "行ってらっしゃい\n鍵は閉めましたか？";
             $beaconMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($beaconMessage);
             $response = $bot->replyMessage($event->replyToken, $beaconMessageBuilder);
         }
+
     }
 ////////////////////////////////////////////////////////////////////////////
 
