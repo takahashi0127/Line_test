cfcc9c6ac76dcbae63964065c1b2121e6307377e
takahashi0127 <justhumannohero0127@gmail.com>
2018-06-20 10:03:44 +0900

    beacon test

--- index.php
+++ index.php
@@ -200,7 +200,7 @@ foreach ($events as $event) {
     //    }//else(text == reg以外)
     }//elseif(text)
 
-    else if ($event->postback->data){//ボタンが押されたとき
+    else if (isPostback($event)){//ボタンが押されたとき
         $botton = $event->postback->data;
         $situation = substr($event->postback->data, -1);
 
